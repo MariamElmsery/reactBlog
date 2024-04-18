014c16c7f05c6f917a2cefb6d643a63cc5ea7f76
mariam <mariamelmsery7@gmail.com>
2024-04-18 16:21:26 +0200

    a

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -42,18 +42,16 @@ export default function Post({ post, currentUser }) {
         </div>
       )}
 
-      <div className="card card-side bg-base-100 shadow-xl mb-2">
-        <figure >
-          <img   src={post.image} alt="img" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{post.title}</h2>
-          <p>{post.description}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
-          </div>
-        </div>
-      </div>
+<div className="card w-7/12 bg-base-100 shadow-xl m-auto image-full mb-3">
+  <figure><img src={post.image} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title text-black font-bold pb-3">{post.title}</h2>
+    <p className="text-black">{post.description}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
 
       {/* <div key={post._id} className="rounded p-2 w-96 m-auto ">
       <img className="rounded " src={post.image} alt="img" />
